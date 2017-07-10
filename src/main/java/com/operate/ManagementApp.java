d69d606313cdff90e2eef2571114230911f32b1c
liuxiaodong <dexia115@sina.com>
2017-07-10 17:13:25 +0800

    优化名称

--- src/main/java/com/operate/ManagementApp.java
+++ src/main/java/com/operate/ManagementApp.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootApp {
+public class ManagementApp {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootApp.class, args);
+		SpringApplication.run(ManagementApp.class, args);
 	}
 }
